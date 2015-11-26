4de0549ed42d72b2f14b9127c7419161c6f7c63c
Remy Dyer (CSIRO) <dye025@csiro.au>
2015-11-27 10:15:06 +1100

    init value behaviour fixed as of cliffordwolf/yosys@a7ffb85

--- example-1k.v
+++ example-1k.v
@@ -21,23 +21,11 @@ module top (
     endfunction
     
     
-    reg [31:0] rng = 32'h00010000; // this _should_ seed the rule 30 rng, but it doesn't - it stays all-off until sel is given a negative pulse.
+    reg [31:0] rng = 32'h00010000; 
     
-    initial
-        rng = 32'h00010000; // ... nope, does nothing too. 
-    // desparate measures!    
-    reg [15:0] autoreset  = 16'h0000;
-    
-    always@(posedge clk)
-        autoreset <= {autoreset[14:0], 1'b1};
-    
-    wire reset = !autoreset[15];  
-    
+
     
     always@(posedge counter[LOG2DELAY-2])
-    if (reset)
-        rng <= 32'h00010000; // still nothing :(
-    else
         rng <= ({rng[0],(rng >> 1)})^(rng | {(rng << 1),rng[31]});
 
     reg [BITS+LOG2DELAY-1:0] counter = 0;
