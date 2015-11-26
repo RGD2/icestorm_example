08f32c8e30a41a183aa3c79e7332401f9e09502b
Remy Dyer (CSIRO) <dye025@csiro.au>
2015-11-26 13:58:35 +1100

    This is has a broken 1k example, which shouldn't be broken except the tools don't seem to be respecting init values

--- example-1k.v
+++ example-1k.v
@@ -20,11 +20,31 @@ module top (
         end
     endfunction
     
+    
+    reg [31:0] rng = 32'h00010000; // this _should_ seed the rule 30 rng, but it doesn't - it stays all-off until sel is given a negative pulse.
+    
+    initial
+        rng = 32'h00010000; // ... nope, does nothing too. 
+    // desparate measures!    
+    reg [15:0] autoreset  = 16'h0000;
+    
+    always@(posedge clk)
+        autoreset <= {autoreset[14:0], 1'b1};
+    
+    wire reset = !autoreset[15];  
+    
+    
+    always@(posedge counter[LOG2DELAY-2])
+    if (reset)
+        rng <= 32'h00010000; // still nothing :(
+    else
+        rng <= ({rng[0],(rng >> 1)})^(rng | {(rng << 1),rng[31]});
+
     reg [BITS+LOG2DELAY-1:0] counter = 0;
     
     always@(posedge clk)
         counter <= counter + 1;
         
-    assign {LED1, LED2, LED3, LED4, LED5} = bin2gray(counter >> LOG2DELAY);
+    assign {LED1, LED2, LED3, LED4, LED5} = rng[11:7];  // = bin2gray(counter >> LOG2DELAY-1);
     
 endmodule 
